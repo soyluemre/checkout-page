17a42babe2a8067a6bea8e74ab284df33e60a8e7
soyluemre <emresoyludev@gmail.com>
2022-09-17 17:20:25 +0300

    finish

---
+++ app.js
@@ -0,0 +1,54 @@
+const tax = 0.18;
+const shipping = 15;
+
+document.querySelector(".products").onclick = (e) => {
+  if (e.target.className === "fas fa-minus") {
+    if (e.target.parentElement.nextElementSibling.innerText > 1) {
+      e.target.parentElement.nextElementSibling.innerText--;
+
+      productTotal(
+        e.target.parentElement.nextElementSibling.innerText,
+        e.target.parentElement.parentElement.parentElement
+      );
+    } else {
+      if (confirm("Produkt wird gelöscht?")) {
+        e.target.parentElement.parentElement.parentElement.parentElement.remove();
+        cartTotal();
+      }
+    }
+  } else if (e.target.className === "fas fa-plus") {
+    e.target.parentElement.previousElementSibling.innerText++;
+    productTotal(
+      e.target.parentElement.previousElementSibling.innerText,
+      e.target.parentElement.parentElement.parentElement
+    );
+  } else if (e.target.className === "removeProduct") {
+    e.target.parentElement.parentElement.parentElement.remove();
+    cartTotal();
+  }
+};
+
+let productTotal = (e, b) => {
+  let productPrice = b.children[1].children[0].firstChild.innerText;
+  let productTotal = b.children[4];
+  let amount = e;
+  productTotal.innerText = (productPrice * amount).toFixed(2);
+
+  cartTotal();
+};
+let cartTotal = () => {
+  let productTotalDivs = document.querySelectorAll(".productLinePrice");
+  let subTotal = 0;
+  productTotalDivs.forEach((e) => {
+    subTotal += parseFloat(e.innerText);
+  });
+  let taxTotal = parseFloat(subTotal * tax).toFixed(2);
+  let overallTotal = parseFloat(subTotal + taxTotal + shipping).toFixed(2);
+  document.getElementById("subTotal").children[1].innerText =
+    subTotal.toFixed(2);
+  document.getElementById("tax").children[1].innerText = taxTotal;
+  document.getElementById("shipping").children[1].innerText = subTotal
+    ? shipping
+    : 0;
+  document.getElementById("overallTotal").children[1].innerText = overallTotal;
+};
